15263b596480f5b523f9fd678c3c3790c3482cd5
charleschen22 <35230041+charleschen22@users.noreply.github.com>
2019-08-05 09:20:40 +0800

    Update train_FCN.py

--- train_FCN.py
+++ train_FCN.py
@@ -25,7 +25,6 @@ import staintools
 import tensorflow as tf
 import keras.backend.tensorflow_backend as KTF
 from preprocessing import get_id_list
-from patches_production import gen_imgs, get_start_coordinate, crop
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "6, 7"
 config = tf.ConfigProto()
@@ -33,7 +32,61 @@ config = tf.ConfigProto()
 sess = tf.Session(config=config)
 KTF.set_session(sess)
 
+def rgba_2_rgb(r, g, b, a, rb=255, gb=255, bb=255):
+    r3 = int((1-a)*rb + a*r)
+    g3 = int((1-a)*gb + a*g)
+    b3 = int((1-a)*bb + a*b)
+
+    return(np.array([r3, g3, b3]))
+
+
+def get_id_list(slides_dir):
+    id_list = []
+    svs_file_list = filesystem.find_ext_files(slides_dir, "svs")
+    id_list.extend([os.path.basename(ele) for ele in svs_file_list])
+    SVS_file_list = filesystem.find_ext_files(slides_dir, "SVS")
+    id_list.extend([os.path.basename(ele) for ele in SVS_file_list])
+    ids = [os.path.splitext(ele)[0] for ele in id_list]
+
+    return ids
+
+
 id_list = get_id_list('./data/OriginalImage')
+
+
+def sequential_crop(img_id, start, crop_size):
+
+    start_x, start_y = start
+    slide_path = './data/OriginalImage/' + str(img_id) + '.svs'
+    if not os.path.exists(slide_path):
+        slide_path = './data/OriginalImage/' + str(img_id) + '.SVS'
+    slide = openslide.open_slide(slide_path)
+    croped_slide_img = slide.read_region((start_x, start_y), 0, crop_size)
+    croped_slide_img = np.array(croped_slide_img)#[:, :, 0:3]
+    mask_path = './data/ViableMask/' + str(img_id) + '_viable.tif'
+    mask = io.imread(mask_path)
+    croped_mask_img = mask[start_x:start_x+crop_size[0], start_y:start_y+crop_size[1]]
+
+    return(croped_slide_img, croped_mask_img, start)
+
+
+def get_start_coordinate(img_id, crop_size):
+    coordinate = pd.DataFrame([])
+    mask_path = './data/ViableMask/' + str(img_id) + '_viable.tif'
+    mask = io.imread(mask_path)
+    mask_shape = np.shape(mask)
+    img_num_wide = mask_shape[0]//crop_size[0]
+    img_num_height = mask_shape[1]//crop_size[1]
+    for i in range(img_num_height):
+        for j in range(img_num_wide):
+            x = j*crop_size[0]
+            y = i*crop_size[1]
+            coordinate = coordinate.append([[str(x), str(y)]])
+    coordinate = coordinate.append([[str(mask_shape[0]-crop_size[1]+1), str(mask_shape[1]-crop_size[0]+1)]])
+    print(coordinate)
+
+    return(coordinate)
+
 
 print('Patches producing can take a while...')
 svs_dir_ = './data/OriginalImage'
@@ -46,28 +99,73 @@ svs_paths.extend(glob.glob(osp.join(svs_dir_, '*.SVS')))
 viable_paths = glob.glob(osp.join(viable_dir_, '*.tif'))
 whole_paths = glob.glob(osp.join(whole_dir_, '*tif'))
 
-if os.path.exists('./data/coordinates.csv'):
-    sample_total = pd.read_csv('./data/coordinates.csv', index_col=0)
-else:
-    sample_total = pd.DataFrame([])
-    i = 0
-    while i < len(id_list):
-        print('Processing data #' + str(i) + '............')
-        patches = get_start_coordinate(id_list[i], (512, 512))
-        patches['id'] = id_list[i]
-        sample_total = sample_total.append(patches, ignore_index=True)
-        i += 1
-    sample_total.to_csv('./data/coordinates.csv')
+'''
+sample_total = pd.DataFrame([])
+i = 0
+while i < len(id_list):
+    print('Processing data #' + str(i) + '............')
+    patches = get_start_coordinate(id_list[i], (512, 512))
+    patches['id'] = id_list[i]
+    sample_total = sample_total.append(patches, ignore_index=True)
 
+    i += 1
 
-sample_total = pd.read_csv('./data/coordinates.csv', index_col=0)
-sample_total = sample_total.rename(columns={'0': 'x', '1': 'y'})
-sample_total[['x']] = sample_total[['x']].astype(str)
-sample_total[['y']] = sample_total[['y']].astype(str)
-train_index = np.random.randint(0, sample_total.shape[0], int(0.8*sample_total.shape[0]))
-valid_index = np.delete(np.array(range(sample_total.shape[0])), train_index)
-train_samples = sample_total.loc[train_index]
-valid_samples = sample_total.loc[valid_index]
+sample_total.to_csv('./data/coordinates.csv')
+'''
+
+def gen_imgs(samples, batch_size, shuffle=True, color_norm=True, target="./data/svs_patches/01_01_0091_12800_22528.png"):
+
+    save_svs_patches = './data/svs_patches'
+    save_viable_patches = './data/viable_patches'
+    num_samples = len(samples)
+    target = staintools.read_image(target)
+
+    while 1:
+        if shuffle:
+            samples = samples.sample(frac=1)
+
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples.iloc[offset:offset + batch_size]
+            images = []
+            masks = []
+            id_list = list(batch_samples['id'])
+            x_list = list(batch_samples['x'])
+            y_list = list(batch_samples['y'])
+
+            for i in range(batch_size):
+                a = id_list[i]
+                print('********** Crop in ' + str(a) + ' **********')
+                cor1 = int(x_list[i])
+                cor2 = int(y_list[i])
+                slide_patch, mask_patch = sequential_crop(a, (cor1, cor2), (512, 512))[0:2]
+
+                if color_norm:
+                    target = staintools.LuminosityStandardizer.standardize(target)
+                    slide_patch = staintools.LuminosityStandardizer.standardize(slide_patch)
+                    normalizer = staintools.StainNormalizer(method='vahadane')
+                    normalizer.fit(target)
+                    slide_patch = normalizer.transform(slide_patch)
+
+                if not os.path.exists(save_svs_patches):
+                    os.mkdir(save_svs_patches)
+                imsave(osp.join(save_svs_patches, str(a) + '_' + str(cor1) + '_' + str(cor2) + '.png'), slide_patch)
+                if not os.path.exists(save_viable_patches):
+                    os.mkdir(save_viable_patches)
+                imsave(osp.join(save_viable_patches, str(a) + '_' + str(cor1) + '_' + str(cor2) + '_viable.png'), mask_patch)
+
+                #mask_patch = np.resize(mask_patch, [512, 512, 1])
+                images.append(slide_patch)
+                masks.append(mask_patch)
+
+                batch_samples = pd.DataFrame(batch_samples)
+
+                X_train = np.array(images)
+                y_train = np.array(masks)
+                y_train = to_categorical(y_train, num_classes=2).reshape(y_train.shape[0], 512, 512, 2)
+                print(np.shape(X_train))
+                print(np.shape(y_train))
+
+            yield X_train, y_train
 
 
 from keras.models import Sequential
@@ -91,11 +189,22 @@ model.add(Conv2DTranspose(2, (31, 31), strides=(16, 16), activation='softmax', p
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+
+sample_total = pd.read_csv('./data/coordinates.csv', index_col=0)
+sample_total = sample_total.rename(columns={'0': 'x', '1': 'y'})
+sample_total[['x']] = sample_total[['x']].astype(str)
+sample_total[['y']] = sample_total[['y']].astype(str)
+train_index = np.random.randint(0, sample_total.shape[0], int(0.8*sample_total.shape[0]))
+valid_index = np.delete(np.array(range(sample_total.shape[0])), train_index)
+train_samples = sample_total.loc[train_index]
+valid_samples = sample_total.loc[valid_index]
+
+
 BATCH_SIZE = 32
 N_EPOCHS = 10
 
 # checkpoint
-filepath = "./data/model/fcn_weight_best.hdf5"
+filepath="./data/model/fcn_weight_best.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 print("Training starts...")
